f5b61b4cb509d9df1db060d08907a39fc4f3c42b
Fusicek <ondra.zury@gmail.com>
2026-01-24 14:35:15 +0100

    Update jobs.js

--- routes/jobs.js
+++ routes/jobs.js
@@ -10,3 +10,15 @@ router.get('/', auth, async (req, res) => {
 });
 
 module.exports = router;
+// Vytvořit novou zakázku
+router.post('/', async (req, res) => {
+  try {
+    const job = await Job.create(req.body);
+    res.json(job);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Nepodařilo se vytvořit zakázku' });
+  }
+});
+
+
