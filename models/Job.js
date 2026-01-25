3a14fcb1738ebe524c5bcf5c66fc341fd8aeb0b4
Fusicek <ondra.zury@gmail.com>
2026-01-25 19:00:43 +0100

    Update Job.js

--- models/Job.js
+++ models/Job.js
@@ -1,12 +1,36 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/db');
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
+import { JobStatus } from "../constants/jobStatus.js";
 
-const Job = sequelize.define('Job', {
-  title: DataTypes.STRING,
-  description: DataTypes.TEXT,
-  price: DataTypes.INTEGER,
-  providerId: DataTypes.INTEGER,
-  customerId: DataTypes.INTEGER
+const Job = sequelize.define("Job", {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+  },
+  mode: {
+    type: DataTypes.ENUM("wait", "choose"),
+    allowNull: false,
+  },
+  status: {
+    type: DataTypes.ENUM(...Object.values(JobStatus)),
+    allowNull: false,
+    defaultValue: JobStatus.NEW_WAITING,
+  },
+  agreedDate: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
+  requesterEmail: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  helperEmail: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
 });
 
-module.exports = { Job };
+export default Job;
