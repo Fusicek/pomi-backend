fce5d3840bb5413578c376e68947e562c6a35e56
Fusicek <ondra.zury@gmail.com>
2026-01-25 01:30:47 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,35 +1,24 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+
 const app = express();
-const sequelize = require('./config/db');
-
-sequelize.authenticate()
-  .then(() => console.log('Database connected'))
-  .catch(err => console.error('Database error:', err));
-
-sequelize.sync();
-
-
-const authRoutes = require('./routes/auth');
-const usersRoutes = require('./routes/users');
-const jobsRoutes = require('./routes/jobs');
-const paymentsRoutes = require('./routes/payments');
-const reviewsRoutes = require('./routes/reviews');
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/users', usersRoutes);
-app.use('/api/jobs', jobsRoutes);
-app.use('/api/payments', paymentsRoutes);
-app.use('/api/reviews', reviewsRoutes);
+// ROUTES
+app.use('/api/jobs', require('./routes/jobs'));
+app.use('/api/location', require('./routes/location'));
 
-const PORT = process.env.PORT || 8080;
+// TEST
+app.get('/', (req, res) => {
+  res.send('Pomi backend běží');
+});
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Backend běží na portu ${PORT}`);
+  console.log(`Server běží na portu ${PORT}`);
 });
 
 
