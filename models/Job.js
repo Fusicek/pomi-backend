f4bf54556dfbadeb330fc4e89a4f5945ac3558f0
Fusicek <ondra.zury@gmail.com>
2026-01-25 21:20:57 +0100

    Update Job.js

--- models/Job.js
+++ models/Job.js
@@ -1,36 +1,53 @@
-import { DataTypes } from "sequelize";
-import sequelize from "../config/db.js";
-import { JobStatus } from "../constants/jobStatus.js";
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/db");
 
 const Job = sequelize.define("Job", {
-  title: {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+
+  type: {
     type: DataTypes.STRING,
     allowNull: false,
   },
-  description: {
-    type: DataTypes.TEXT,
+
+  location: {
+    type: DataTypes.STRING,
+    allowNull: false,
   },
+
+  date: {
+    type: DataTypes.DATEONLY,
+    allowNull: false,
+  },
+
+  timeFrom: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  timeTo: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  reward: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+
   mode: {
-    type: DataTypes.ENUM("wait", "choose"),
+    type: DataTypes.STRING, // wait | choose
     allowNull: false,
   },
+
   status: {
-    type: DataTypes.ENUM(...Object.values(JobStatus)),
-    allowNull: false,
-    defaultValue: JobStatus.NEW_WAITING,
-  },
-  agreedDate: {
-    type: DataTypes.DATE,
-    allowNull: true,
-  },
-  requesterEmail: {
     type: DataTypes.STRING,
     allowNull: false,
-  },
-  helperEmail: {
-    type: DataTypes.STRING,
-    allowNull: true,
+    defaultValue: "čeká na pomocníka",
   },
 });
 
-export default Job;
+module.exports = Job;
