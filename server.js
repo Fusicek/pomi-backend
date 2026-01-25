d30717c83ed16ad698c5dcaa9c4e000b1023727e
Fusicek <ondra.zury@gmail.com>
2026-01-25 20:31:15 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,21 +2,25 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const sequelize = require("./config/db");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// test endpoint
-app.get("/", (req, res) => {
-  res.json({ status: "OK", message: "Backend běží" });
-});
-
 // ROUTES
+app.use("/api/users", require("./routes/users"));
 app.use("/api/jobs", require("./routes/jobs"));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log("Backend běží na portu", PORT);
+// TEST
+app.get("/", (req, res) => {
+  res.json({ status: "Backend běží" });
+});
+
+sequelize.sync().then(() => {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log("Server běží na portu", PORT);
+  });
 });
