8ec2bf7bc3f8da0ab0f372af516e6e81be03bff1
Fusicek <ondra.zury@gmail.com>
2026-01-26 15:23:15 +0100

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,40 +1,9 @@
-const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = require("../config/db");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
-  protocol: "postgres",
-  logging: false,
-});
+// pouze načteme modely, NIC NEVOLÁME
+require("./User");
+require("./Job");
 
-// ===== MODELS =====
-const User = require("./User")(sequelize, DataTypes);
-const Job = require("./Job")(sequelize, DataTypes);
-const Chat = require("./Chat")(sequelize, DataTypes);
-const Message = require("./Message")(sequelize, DataTypes);
-const Review = require("./Review")(sequelize, DataTypes);
-
-// ===== ASSOCIATIONS =====
-User.hasMany(Job, { foreignKey: "customerId" });
-Job.belongsTo(User, { foreignKey: "customerId", as: "customer" });
-
-User.hasMany(Job, { foreignKey: "providerId" });
-Job.belongsTo(User, { foreignKey: "providerId", as: "provider" });
-
-Job.hasMany(Chat);
-Chat.belongsTo(Job);
-
-Chat.hasMany(Message);
-Message.belongsTo(Chat);
-
-User.hasMany(Review);
-Review.belongsTo(User);
-
-// ===== EXPORT =====
 module.exports = {
   sequelize,
-  User,
-  Job,
-  Chat,
-  Message,
-  Review,
 };
