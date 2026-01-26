6db00d394f1e450000b3c8f172e60344f68b9430
Fusicek <ondra.zury@gmail.com>
2026-01-26 17:34:24 +0100

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,12 +1,40 @@
 const { Sequelize, DataTypes } = require("sequelize");
 const sequelize = require("../config/db");
 
-const User = require("./User")(sequelize, DataTypes);
-const Job = require("./Job")(sequelize, DataTypes);
+const User = require("./User");
+const Job = require("./Job");
+const Chat = require("./Chat");
+const Message = require("./Message");
+const Review = require("./Review");
+
+/* ===== INIT MODELS ===== */
+User.initModel(sequelize, DataTypes);
+Job.initModel(sequelize, DataTypes);
+Chat.initModel(sequelize, DataTypes);
+Message.initModel(sequelize, DataTypes);
+Review.initModel(sequelize, DataTypes);
+
+/* ===== ASSOCIATIONS ===== */
+User.hasMany(Job, { foreignKey: "customerId" });
+User.hasMany(Job, { foreignKey: "providerId" });
+
+Job.belongsTo(User, { as: "customer", foreignKey: "customerId" });
+Job.belongsTo(User, { as: "provider", foreignKey: "providerId" });
+
+Chat.belongsTo(Job);
+Job.hasOne(Chat);
+
+Chat.hasMany(Message);
+Message.belongsTo(Chat);
+
+User.hasMany(Review);
+Review.belongsTo(User);
 
 module.exports = {
   sequelize,
-  Sequelize,
   User,
   Job,
+  Chat,
+  Message,
+  Review,
 };
