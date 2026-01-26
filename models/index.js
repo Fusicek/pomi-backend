a34dc49dfd7a7a944f282aff23b4bb81e466ece8
Fusicek <ondra.zury@gmail.com>
2026-01-26 19:20:06 +0100

    Update index.js

--- models/index.js
+++ models/index.js
@@ -3,11 +3,8 @@ const sequelize = require("../db");
 const User = require("./User")(sequelize);
 const Job = require("./Job")(sequelize);
 
-User.hasMany(Job);
-Job.belongsTo(User);
-
 module.exports = {
   sequelize,
   User,
-  Job
+  Job,
 };
