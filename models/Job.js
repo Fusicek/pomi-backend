0d52aed7a51a277e2c7613e4e049ece9b2f65aad
Fusicek <ondra.zury@gmail.com>
2026-01-28 00:20:03 +0100

    Update Job.js

--- models/Job.js
+++ models/Job.js
@@ -4,35 +4,48 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
+
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
     description: {
       type: DataTypes.TEXT,
       allowNull: false,
     },
+
     reward: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+
     date: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+
     timeFrom: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+
     timeTo: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+
     location: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+
     status: {
       type: DataTypes.STRING,
       allowNull: false,
       defaultValue: "cekani",
     },
+
     customerId: {
       type: DataTypes.INTEGER,
       allowNull: false,
