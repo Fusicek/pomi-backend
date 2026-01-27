b0ee132863149fcd12963fb6038b768e0edf3eea
Fusicek <ondra.zury@gmail.com>
2026-01-27 18:52:04 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,28 +2,19 @@ const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
   return sequelize.define("User", {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-
     email: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
     },
-
     password: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-
     role: {
       type: DataTypes.ENUM("zadavatel", "zhotovitel"),
       allowNull: false,
