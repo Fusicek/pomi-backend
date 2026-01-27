bf14db36b1cee640cc8e16b30eef1067e1bf261c
Fusicek <ondra.zury@gmail.com>
2026-01-27 19:37:13 +0100

    Update Job.js

--- models/Job.js
+++ models/Job.js
@@ -1,39 +1,56 @@
-const { DataTypes } = require("sequelize");
-
-module.exports = (sequelize) => {
-  return sequelize.define("Job", {
+module.exports = (sequelize, DataTypes) => {
+  const Job = sequelize.define("Job", {
     title: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+
     category: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+
+    reward: {
+      type: DataTypes.STRING, // orientační odměna
+      allowNull: false,
+    },
+
     date: {
       type: DataTypes.DATEONLY,
       allowNull: false,
     },
+
     timeFrom: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.INTEGER, // 1–24
       allowNull: false,
     },
+
     timeTo: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.INTEGER, // 1–24
       allowNull: false,
     },
+
     location: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    reward: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
+
     status: {
       type: DataTypes.STRING,
       defaultValue: "cekani",
     },
+
+    customerId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
   });
+
+  return Job;
 };
 
