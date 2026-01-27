9b22b88b9a840735cc8d37732a00ba3abc33ac94
Fusicek <ondra.zury@gmail.com>
2026-01-28 00:12:38 +0100

    Update index.js

--- models/index.js
+++ models/index.js
@@ -8,10 +8,6 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
 const User = require("./User")(sequelize, DataTypes);
 const Job = require("./Job")(sequelize, DataTypes);
 
-// VAZBY
-User.hasMany(Job, { foreignKey: "customerId" });
-Job.belongsTo(User, { foreignKey: "customerId" });
-
 module.exports = {
   sequelize,
   User,
