a3e44b680c2642abdd3514f66c17883ee4779567
Fusicek <ondra.zury@gmail.com>
2026-01-27 17:50:23 +0100

    Update Job.js

--- models/Job.js
+++ models/Job.js
@@ -1,51 +1,56 @@
-const { DataTypes } = require("sequelize");
+const { DataTypes, Model } = require("sequelize");
 
-module.exports = (sequelize) => {
-  return sequelize.define("Job", {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
+class Job extends Model {
+  static initModel(sequelize) {
+    Job.init(
+      {
+        title: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
 
-    type: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
+        description: {
+          type: DataTypes.TEXT,
+          allowNull: true,
+        },
 
-    date: {
-      type: DataTypes.DATEONLY,
-      allowNull: false,
-    },
+        date: {
+          type: DataTypes.DATEONLY,
+          allowNull: false,
+        },
 
-    timeFrom: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
+        timeFrom: {
+          type: DataTypes.INTEGER, // hodiny 1–24
+          allowNull: false,
+        },
 
-    timeTo: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
+        timeTo: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+        },
 
-    reward: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
+        location: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
 
-    location: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
+        estimatedReward: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
 
-    mode: {
-      type: DataTypes.STRING, // "cekat" | "vybrat"
-      allowNull: false,
-    },
+        status: {
+          type: DataTypes.ENUM("cekajici", "domluveno", "hotovo"),
+          defaultValue: "cekajici",
+        },
+      },
+      {
+        sequelize,
+        modelName: "Job",
+      }
+    );
+  }
+}
 
-    status: {
-      type: DataTypes.STRING,
-      defaultValue: "nova",
-    }
-  });
-};
+module.exports = Job;
+
