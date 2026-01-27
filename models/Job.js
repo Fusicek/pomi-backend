eef0449443b4d37d2d15c411146ed1d1511d2c9f
Fusicek <ondra.zury@gmail.com>
2026-01-27 18:52:21 +0100

    Update Job.js

--- models/Job.js
+++ models/Job.js
@@ -1,55 +1,39 @@
-const { DataTypes, Model } = require("sequelize");
+const { DataTypes } = require("sequelize");
 
-class Job extends Model {
-  static initModel(sequelize) {
-    Job.init(
-      {
-        title: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-        category: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-        date: {
-          type: DataTypes.DATEONLY,
-          allowNull: false,
-        },
-        timeFrom: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-        },
-        timeTo: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-        },
-        location: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-        reward: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-        status: {
-          type: DataTypes.ENUM(
-            "cekani",
-            "nabidka",
-            "domluveno",
-            "hotovo"
-          ),
-          defaultValue: "cekani",
-        },
-      },
-      {
-        sequelize,
-        modelName: "Job",
-        tableName: "Jobs",
-      }
-    );
-  }
-}
-
-module.exports = Job;
+module.exports = (sequelize) => {
+  return sequelize.define("Job", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    timeFrom: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    timeTo: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    reward: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.STRING,
+      defaultValue: "cekani",
+    },
+  });
+};
 
