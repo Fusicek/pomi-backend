e0d27d5be09b059c8131f9521843a49d983f7967
Fusicek <ondra.zury@gmail.com>
2026-01-28 15:23:57 +0100

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,15 +1,14 @@
-const { Sequelize, DataTypes } = require("sequelize");
+const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
+  protocol: "postgres",
   logging: false,
 });
 
-const User = require("./User")(sequelize, DataTypes);
-const Job = require("./Job")(sequelize, DataTypes);
+const Job = require("./Job")(sequelize);
 
 module.exports = {
   sequelize,
-  User,
   Job,
 };
