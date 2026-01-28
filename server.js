160e38652181a57aad35a35cc97afc71f6273f6b
Fusicek <ondra.zury@gmail.com>
2026-01-28 23:14:59 +0100

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,14 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   protocol: "postgres",
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
+
 
 /* =========================
    MODELS
