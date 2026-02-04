076cacf8d3da0064c74bfeb145c1c9b84e9ec1d8
Fusicek <ondra.zury@gmail.com>
2026-02-05 00:42:47 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,18 +4,32 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
+
     email: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
     },
+
     password: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+
     role: {
       type: DataTypes.STRING, // zadavatel | zhotovitel
       allowNull: false,
+    },
+
+    // 🆕 PROFIL ZHOTOVITELE
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+
+    profileCompleted: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
   });
 };
