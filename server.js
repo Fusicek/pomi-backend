16003d54adb5376ae5c96b81499d1711b131c4cb
Fusicek <ondra.zury@gmail.com>
2026-02-16 21:12:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -101,24 +101,21 @@ const JobRating = sequelize.define("JobRating", {
   comment: DataTypes.TEXT,
 });
 const Notification = sequelize.define("Notification", {
-  userId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-
   type: {
     type: DataTypes.STRING,
     allowNull: false,
   },
-
   message: {
     type: DataTypes.STRING,
     allowNull: false,
   },
-
   isRead: {
     type: DataTypes.BOOLEAN,
     defaultValue: false,
+  },
+  userId: {                    // 👈 TOTO TAM MUSÍ BÝT
+    type: DataTypes.INTEGER,
+    allowNull: true,           // 👈 zatím true kvůli Railway
   },
 });
 
