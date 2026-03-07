36a746b3514533ff46ce81a8506b54f6a210950e
Fusicek <ondra.zury@gmail.com>
2026-03-07 17:20:36 +0100

    Update chatSocket.js

--- socket/chatSocket.js
+++ socket/chatSocket.js
@@ -3,59 +3,51 @@ const User = require("../models/User");
 
 module.exports = function(io){
 
-io.on("connection",(socket)=>{
+  io.on("connection",(socket)=>{
 
-console.log("🔌 User connected:",socket.id);
+    console.log("🔌 User connected:", socket.id);
 
-socket.on("joinJob",(jobId)=>{
+    socket.on("joinJob",(jobId)=>{
+      socket.join(`job_${jobId}`);
+    });
 
-socket.join(`job_${jobId}`);
+    socket.on("send_message", async (data)=>{
 
-});
+      try{
 
-socket.on("send_message", async(data)=>{
+        const { jobId, userId, text } = data;
 
-try{
+        const message = await ChatMessage.create({
+          jobId,
+          userId,
+          message:text
+        });
 
-const {jobId,userId,text} = data;
+        const msg = await ChatMessage.findByPk(message.id,{
+          include:[
+            { model:User, as:"sender", attributes:["id","name"] }
+          ]
+        });
 
-const message = await ChatMessage.create({
+        io.to(`job_${jobId}`).emit("receive_message",{
+          id: msg.id,
+          jobId,
+          userId,
+          text: msg.message,
+          sender: msg.sender?.name,
+          createdAt: msg.createdAt
+        });
 
-jobId,
-userId,
-message:text
+      }catch(err){
+        console.error("Chat error:", err);
+      }
 
-});
+    });
 
-const msg = await ChatMessage.findByPk(message.id,{
-include:[{model:User,as:"sender",attributes:["id","name"]}]
-});
+    socket.on("disconnect",()=>{
+      console.log("❌ User disconnected:", socket.id);
+    });
 
-io.to(`job_${jobId}`).emit("receive_message",{
-
-id:msg.id,
-jobId,
-userId,
-text:msg.message,
-sender:msg.sender?.name,
-createdAt:msg.createdAt
-
-});
-
-}catch(err){
-
-console.error("Chat error:",err);
-
-}
-
-});
-
-socket.on("disconnect",()=>{
-
-console.log("❌ User disconnected:",socket.id);
-
-});
-
-});
+  });
 
 };
