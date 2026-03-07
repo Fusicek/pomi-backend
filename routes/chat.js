aac868f51adbf5c2191fbb8f38c0cf550d26c063
Fusicek <ondra.zury@gmail.com>
2026-03-07 17:11:52 +0100

    Create chat.js

---
+++ routes/chat.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const router = express.Router();
+
+const ChatMessage = require("../models/ChatMessage");
+const User = require("../models/User");
+
+const { requireUser } = require("../middleware/auth");
+
+router.get("/:jobId/messages",requireUser, async(req,res)=>{
+
+  const messages = await ChatMessage.findAll({
+
+    where:{jobId:req.params.jobId},
+
+    include:[
+      {
+        model:User,
+        as:"sender",
+        attributes:["id","name"]
+      }
+    ],
+
+    order:[["createdAt","ASC"]]
+
+  });
+
+  res.json(messages);
+
+});
+
+router.post("/:jobId/messages",requireUser, async(req,res)=>{
+
+  const {message} = req.body;
+
+  const newMessage = await ChatMessage.create({
+
+    jobId:req.params.jobId,
+    userId:req.user.id,
+    message
+
+  });
+
+  res.json(newMessage);
+
+});
+
+module.exports = router;
